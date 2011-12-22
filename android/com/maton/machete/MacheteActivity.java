4c25b71210c91289c0f8891c9b22cd24d83ba9a9
Hilario Pérez Corona <hpcorona@gmail.com>
2011-12-22 01:54:57 -0800

    * OpenGL ES 2.0 activity configuration on Android.

--- android/com/maton/machete/MacheteActivity.java
+++ android/com/maton/machete/MacheteActivity.java
@@ -1,6 +1,9 @@
 package com.maton.machete;
 
+import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.app.Activity;
@@ -8,6 +11,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.EGLConfigChooser;
+import android.opengl.GLSurfaceView.EGLContextFactory;
 import android.opengl.GLSurfaceView.Renderer;
 import android.os.Bundle;
 import android.view.Window;
@@ -19,7 +24,7 @@ public abstract class MacheteActivity extends Activity {
 	private MacheteNative engine;
 	private boolean initialized = false;
 	private String baseAssets;
-	
+
 	protected MacheteActivity(String baseAssets) {
 		this.baseAssets = baseAssets;
 		engine = new MacheteNative();
@@ -29,13 +34,54 @@ public abstract class MacheteActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		fullScreen();
 		hideTitle();
-		
-		mGLSurfaceView = new GLSurfaceView(this);
-		mGLSurfaceView.setRenderer(new Renderer() {
 
+		mGLSurfaceView = new GLSurfaceView(this);
+
+		mGLSurfaceView.setEGLConfigChooser(new EGLConfigChooser() {
+			public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
+				EGLConfig[] configs = new EGLConfig[1];
+				int[] num_config = new int[1];
+
+				boolean check = false;
+
+				int[] configSpec = { EGL10.EGL_DEPTH_SIZE, 16, EGL10.EGL_NONE };
+
+				check = egl.eglInitialize(display, new int[] { 2, 0 });
+
+				if (!check)
+					return null;
+				check = false;
+
+				check = egl.eglChooseConfig(display, configSpec, configs, 1,
+						num_config);
+				if (!check)
+					return null;
+
+				return configs[0];
+			}
+		});
+
+		mGLSurfaceView.setEGLContextFactory(new EGLContextFactory() {
+			public void destroyContext(EGL10 egl, EGLDisplay display,
+					EGLContext context) {
+				egl.eglDestroyContext(display, context);
+			}
+
+			public EGLContext createContext(EGL10 egl, EGLDisplay display,
+					EGLConfig eglConfig) {
+				int[] attrib_list = new int[] { 0x3098 /*EGL10.EGL_VERSION*/, 2,
+						EGL10.EGL_NONE };
+
+				EGLContext context = egl.eglCreateContext(display, eglConfig,
+						EGL10.EGL_NO_CONTEXT, attrib_list);
+				return context;
+			}
+		});
+
+		mGLSurfaceView.setRenderer(new Renderer() {
 			public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 			}
 
@@ -47,8 +93,7 @@ public abstract class MacheteActivity extends Activity {
 					ApplicationInfo appInfo = null;
 					PackageManager packMgmr = getPackageManager();
 					try {
-						appInfo = packMgmr.getApplicationInfo(
-								baseAssets, 0);
+						appInfo = packMgmr.getApplicationInfo(baseAssets, 0);
 					} catch (NameNotFoundException e) {
 						e.printStackTrace();
 						throw new RuntimeException(
@@ -104,4 +149,5 @@ public abstract class MacheteActivity extends Activity {
 	}
 
 	private GLSurfaceView mGLSurfaceView;
+
 }
